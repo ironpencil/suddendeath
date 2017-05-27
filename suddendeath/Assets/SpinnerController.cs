b067f01424e9f4a941347f61db55c03155d5f7f1
Sean Smith <seansmith@fedex.com>
2017-05-27 14:16:12 -0400

    Added spinner

---
+++ suddendeath/Assets/SpinnerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerController : MonoBehaviour {
+    public float MoveSpeed = 8.0f;
+    public float SpinSpeed = 8.0f;
+    public float MaxAngularVelocity = 4.0f;
+
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(Random.Range(-1.0f, 1.0f) * MoveSpeed, Random.Range(-1.0f, 1.0f) * MoveSpeed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        Vector2 direction = rb2d.velocity.normalized;
+        rb2d.velocity = direction * MoveSpeed;
+        if (rb2d.angularVelocity < MaxAngularVelocity)
+        {
+            rb2d.AddTorque(SpinSpeed);
+        }
+    }
+}
