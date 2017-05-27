c277afb0a07bb845bbfb747250fc69eb7eed4e17
ironpencil <james.a.south@gmail.com>
2017-05-27 11:43:16 -0400

    Flag player when in a pit

---
+++ suddendeath/Assets/BoundsChecker.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BoundsChecker : MonoBehaviour {
+
+    public List<Transform> bounds;
+
+    List<Collider2D> pitColliders;
+
+    SpriteRenderer playerSprite;
+
+	// Use this for initialization
+	void Start () {
+        pitColliders = new List<Collider2D>();
+        playerSprite = gameObject.GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        CheckBounds();
+        pitColliders.Clear();
+    }
+
+    private void CheckBounds()
+    {
+        //check to see if all of our bound points are overlapped by at least one collider
+        bool inPit = bounds.All(p => pitColliders.Any(c => c.OverlapPoint(p.position)));
+        
+        if (inPit)
+        {
+            playerSprite.color = Color.red;
+        } else
+        {
+            playerSprite.color = Color.white;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Pit"))
+        {
+            pitColliders.Add(other);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag.Equals("Pit"))
+        {
+            pitColliders.Add(other);
+        }
+    }
+}
