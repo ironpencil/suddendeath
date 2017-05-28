831fb085390a978092c126dd41e805d7442f307e
Sean Smith <seansmith@fedex.com>
2017-05-28 15:17:16 -0400

    Goofing with the shield

--- suddendeath/Assets/Scripts/PlayerInput.cs
+++ suddendeath/Assets/Scripts/PlayerInput.cs
@@ -6,6 +6,7 @@ public class PlayerInput : MonoBehaviour {
     public float MoveSpeed = 8.0f;
     public int playerNum = 1;
     public bool UseForces = false;
+    public GameObject hand;
 
     Rigidbody2D rb2d;
 
@@ -35,6 +36,7 @@ public class PlayerInput : MonoBehaviour {
         float horizontal = 0.0f;
         float vertical = 0.0f;
 
+
         horizontal = Input.GetAxisRaw("Horizontal" + playerNum);
         vertical = Input.GetAxisRaw("Vertical" + playerNum);
 
@@ -47,6 +49,25 @@ public class PlayerInput : MonoBehaviour {
         {
             rb2d.velocity = moveDirection;
         }
-        
+
+
+        // We are going to read the input every frame
+        Vector2 rinput = new Vector2(Input.GetAxisRaw("RHorizontal" + playerNum), Input.GetAxisRaw("RVertical" + playerNum));
+
+        Debug.Log("RHorizontal" + playerNum + ": " + rinput.x);
+        Debug.Log("RVertical" + playerNum + ": " + rinput.y);
+
+
+        // Only do work if meaningful
+        if (rinput.sqrMagnitude < 0.1f)
+        {
+            return;
+        }
+
+        // Apply the transform to the object  
+        var angle = Mathf.Atan2(rinput.y, rinput.x) * Mathf.Rad2Deg;
+        //hand.transform.Rotate(new Vector3(0, 0, angle));
+        //hand.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        hand.transform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
---
+++ suddendeath/Assets/ShieldScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour {
+    Vector2 offset;
+    Vector2 resetPos;
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        //resetPos = transform.localPosition;
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        offset = transform.localPosition;
+	}
+	
+	void FixedUpdate () {
+        //transform.localPosition = resetPos;
+        //transform.eulerAngles = Vector3.zero;
+        rb2d.position = (Vector2)transform.parent.position + offset;
+        rb2d.rotation = 0;
+	}
+}
