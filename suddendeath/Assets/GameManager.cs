531ccfb2eb3ac13fa187eaf48a1e214e77ca398c
Sean Smith <seansmith@fedex.com>
2017-05-31 11:25:44 -0400

    Added FPS monitor, and Mine image

--- suddendeath/Assets/GameManager.cs
+++ suddendeath/Assets/GameManager.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    float deltaTime = 0.0f;
+    public bool displayFrameRate = false;
 
     public Dictionary<int, PlayerController> players;
     public List<GameObject> spinners;
@@ -58,8 +60,29 @@ public class GameManager : MonoBehaviour {
         scoreScreenUI.SetActive(true);
     }
 
+    void OnGUI()
+    {
+        if (displayFrameRate)
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 100;
+            style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
+    }
+
     // Update is called once per frame
     void Update () {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
         if (isRoundActive)
         {
             List<PlayerController> livingPlayers = players.Values.ToList();
