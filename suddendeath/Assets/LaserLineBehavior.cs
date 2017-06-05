1c840ba32afc8747e91dfedc269ddf55290dcddc
Sean Smith <seansmith@fedex.com>
2017-06-05 19:45:45 -0400

    Added laser line

---
+++ suddendeath/Assets/LaserLineBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLineBehavior : MonoBehaviour {
+    public int speed;
+    public bool isVertical;
+    public Rigidbody2D rb2d;
+    
+    public Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb2d.velocity = direction * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        WallBehavior wb = collider.gameObject.GetComponent<WallBehavior>();
+        
+        if (wb != null)
+        {
+            speed = -speed;
+        } else
+        {
+            PlayerController pc = collider.gameObject.GetComponent<PlayerController>();
+
+            if (pc != null)
+            {
+                pc.Kill(PlayerController.KillType.Dissolve);
+            }
+        }
+    }
+}
